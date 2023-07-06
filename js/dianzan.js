432f9c3b9cddcf8e0e90b8615cb1337886159695
liuyichen-doc <1395264153@qq.com>
2023-07-06 20:41:52 +0800

    Site updated: 2023-07-06 20:41:47

--- js/dianzan.js
+++ js/dianzan.js
@@ -1,9 +1,4 @@
 
-import cloudbase from "@cloudbase/js-sdk";
-
-
-
-
 function GetUrlRelativePath() {
     var url = document.location.toString();
     var arrUrl = url.split("//");
@@ -16,12 +11,25 @@ function GetUrlRelativePath() {
     }
     return relUrl;
 }
+
 $(document).ready(function () {
     const app = cloudbase.init({
         env: "hexo-blog-1gs1lv1j2bf8c35a",//自行替换
         region: "ap-shanghai",//自行替换
     });
 
+    app.callFunction({
+        // 云函数名称
+        name: "like_for_hexo",
+        // 传给云函数的参数
+        data: {
+        a: 1,
+        },
+    })
+    .then((res) => {
+        console.log(res);//成功返回结果
+    })
+    .catch(console.error);//错误返回结果
     // const {
     //     Query,
     //     User
@@ -73,6 +81,10 @@ function setCount() {
 }
 
 function dianzan() {
+    const app = cloudbase.init({
+        env: "hexo-blog-1gs1lv1j2bf8c35a",//自行替换
+        region: "ap-shanghai",//自行替换
+    });
     app.callFunction({
         // 云函数名称
         name: "like_for_hexo",
